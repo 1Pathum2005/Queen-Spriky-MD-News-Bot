d89cca025b29fe7fbfd06cae3900c7b563373ea9
1Pathum2005 <Ppathum068@gmail.com>
2024-09-29 19:08:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', // Enter Your Session ID
+    SESSION_ID: process.env.SESSION_ID || 'RnkmCIiK#CMcNc4XjbmWe3yQLuby9CYzLHhpBRPCs_3OROA1ah3M', // Enter Your Session ID
     MONGODB: process.env.MONGODB || '',    // Enter Your MongoDB URL
-    Owner: process.env.OwnerNumber || '94758900210',    // Enter Your Owner Number
-    BotNumber: process.env.BotNumber || '94773366833'    // Enter Your Bot Number
+    Owner: process.env.OwnerNumber || '94756106661',    // Enter Your Owner Number
+    BotNumber: process.env.BotNumber || '94756106661'    // Enter Your Bot Number
 };
